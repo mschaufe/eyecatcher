c81cb2f2b35a8ccbc62e70ba9282a308ba4a4bb9
mschaufe <marc@schufi.ch>
2020-03-06 06:09:39 +0100

    add eyeDetection example

---
+++ eyeDetectionExample/cmake-build-debug/CMakeFiles/eyeDetection.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/eyeDetection.dir/main.cpp.o"
+  "eyeDetection"
+  "eyeDetection.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/eyeDetection.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
