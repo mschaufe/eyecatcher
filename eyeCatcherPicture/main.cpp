de41c050d6e323dd23f2d64ffb394b6ff680b5c3
marc <mschaufe>
2020-03-12 11:09:20 +0100

    delete simile detection in example

--- eyeCatcherPicture/main.cpp
+++ eyeCatcherPicture/main.cpp
@@ -10,15 +10,14 @@ int main( )
     cv::Mat image;
     cv::Mat image_gray;
     image = cv::imread("/Users/marc/Desktop/Bild2.jpeg", CV_LOAD_IMAGE_COLOR);
-    //   namedWindow( "window1", 1 ); //  imshow( "window1", image );
 
     cv::CascadeClassifier face_cascade;
     cv::CascadeClassifier eyes_cascade;
-    //CascadeClassifier smile_cascade;
+
 
     face_cascade.load( "/usr/local/include/opencv2/data/haarcascades/haarcascade_frontalface_alt.xml" );
     eyes_cascade.load( "/usr/local/include/opencv2/data/haarcascades/haarcascade_eye_tree_eyeglasses.xml");
-    //smile_cascade.load("/usr/local/share/OpenCV/haarcascades/haarcascade_smile.xml");
+
 // Detect faces
     std::vector<cv::Rect> faces;
     cvtColor(image, image_gray, cv::COLOR_BGR2GRAY);
@@ -40,15 +39,6 @@ int main( )
             circle( image,eye_center,radius,cv::Scalar(255, 0, 0),4,8,0 );
         }
 
-        /*std::vector<Rect> smile;
-        smile_cascade.detectMultiScale(faceROI, smile, 1.1, 2, 0|CV_HAAR_SCALE_IMAGE, Size(80,80));
-        for(int j = 0; j < smile.size(); j++)
-        {
-            Point smile_center(faces[i].x + smile[j].x + smile[j].width/2,  faces[i].y + smile[j].y + smile[j].height/2);
-            int radius = cvRound((smile[j].width+smile[j].height)*0.25);
-            circle( image,smile_center,radius,Scalar(0, 255, 0),4,8,0 );
-
-        }*/
     }
     imshow( "Detected Face", image );
 
