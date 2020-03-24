215e8e9c9cb97a41075004983d26d1ba76cea5d8
marc <mschaufe>
2020-03-24 11:00:19 +0100

    add eyeLike

---
+++ eyeLike/cmake-build-debug/src/CMakeFiles/eyeLike.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/marc/Documents/GitHub/eyecatcher/eyeLike/src/findEyeCenter.cpp" "/Users/marc/Documents/GitHub/eyecatcher/eyeLike/cmake-build-debug/src/CMakeFiles/eyeLike.dir/findEyeCenter.cpp.o"
+  "/Users/marc/Documents/GitHub/eyecatcher/eyeLike/src/findEyeCorner.cpp" "/Users/marc/Documents/GitHub/eyecatcher/eyeLike/cmake-build-debug/src/CMakeFiles/eyeLike.dir/findEyeCorner.cpp.o"
+  "/Users/marc/Documents/GitHub/eyecatcher/eyeLike/src/helpers.cpp" "/Users/marc/Documents/GitHub/eyecatcher/eyeLike/cmake-build-debug/src/CMakeFiles/eyeLike.dir/helpers.cpp.o"
+  "/Users/marc/Documents/GitHub/eyecatcher/eyeLike/src/main.cpp" "/Users/marc/Documents/GitHub/eyecatcher/eyeLike/cmake-build-debug/src/CMakeFiles/eyeLike.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/include"
+  "/usr/local/include/opencv"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ eyeLike/cmake-build-debug/src/CMakeFiles/eyeLike.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "../bin/eyeLike"
+  "../bin/eyeLike.pdb"
+  "CMakeFiles/eyeLike.dir/findEyeCenter.cpp.o"
+  "CMakeFiles/eyeLike.dir/findEyeCorner.cpp.o"
+  "CMakeFiles/eyeLike.dir/helpers.cpp.o"
+  "CMakeFiles/eyeLike.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/eyeLike.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
